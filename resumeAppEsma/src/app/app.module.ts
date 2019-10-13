8f9d55d4329d7af26d7125426c84a67dc0d6e65d
eherenda1 <eherenda1@etf.unsa.ba>
2019-10-13 14:12:41 +0200

    lista uredjena

--- resumeAppEsma/src/app/app.module.ts
+++ resumeAppEsma/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonsComponent} from './buttons.component';
-
+import {AboutComponent} from './about.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
